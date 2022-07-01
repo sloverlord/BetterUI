42132eeb4c2e53ba038f0dd86eb6ec9841324eb5
sloverlord <alexpeck99@aol.com>
2022-07-01 19:14:45 -0400

    Add files via upload
    Bug fix for manual resume

--- Button.cs
+++ Button.cs
@@ -20,6 +20,13 @@ public class CustomInputs
     }
 
     [HarmonyPostfix]
+    [HarmonyPatch(typeof(PauseState), "OnResume")]
+    private static void PauseStateOnResumePostPatch()
+    {
+        PausePatchManager.Instance.isPaused = false;
+    }
+
+    [HarmonyPostfix]
     [HarmonyPatch(typeof(GameController), "Start")]
     private static void GameControllerStartPostPatch(GameController __instance)
     {
@@ -45,7 +52,6 @@ public class CustomInputs
                 if (PausePatchManager.Instance.isPaused){
                     if (gameController.CurrentState is PauseState){
                         gameController.pauseResumeButton.onClick.Invoke();
-                        PausePatchManager.Instance.isPaused = false;
                     } else if (gameController.CurrentState is OptionsState){
                         gameController.optionsBackButton.onClick.Invoke();
                     } else if (gameController.CurrentState is SynergyUIState){
