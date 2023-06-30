1289fc0566fdd8f880fd2e4cff255f048aa304f2
NeoKaios <mdgscode@mailo.com>
2023-06-30 21:28:34 +0100

    fix: fixed mod for 1.0

--- Button.cs
+++ Button.cs
@@ -13,58 +13,58 @@ namespace BetterUI;
 // Allow closing menus with escape
 public static class CustomInputs
 {
-	private static GameController gameController = null;
-	private static bool paused = false;
+    private static GameController gameController = null;
+    private static bool paused = false;
 
-	[HarmonyPostfix]
-	[HarmonyPatch(typeof(PauseState), "Enter")]
-	private static void PauseStateEnterPostPatch(PauseState __instance)
-	{
-		if(!paused)
-			__instance.StartCoroutine(WaitForPause());
-	}
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PauseState), "Enter")]
+    private static void PauseStateEnterPostPatch(PauseState __instance)
+    {
+        if (!paused)
+            __instance.StartCoroutine(WaitForPause());
+    }
 
-	private static IEnumerator WaitForPause()
-	{
-		yield return new WaitForEndOfFrame();
+    private static IEnumerator WaitForPause()
+    {
+        yield return new WaitForEndOfFrame();
 
-		paused = true;
-	}
+        paused = true;
+    }
 
-	[HarmonyPostfix]
-	[HarmonyPatch(typeof(PauseState), "OnResume")]
-	private static void PauseStateOnResumePostPatch()
-	{
-		paused = false;
-	}
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PauseState), "OnResume")]
+    private static void PauseStateOnResumePostPatch()
+    {
+        paused = false;
+    }
 
-	[HarmonyPostfix]
-	[HarmonyPatch(typeof(GameController), "Start")]
-	private static void GameControllerStartPostPatch(GameController __instance)
-	{
-		gameController = __instance;
-	}
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameController), "Start")]
+    private static void GameControllerStartPostPatch(GameController __instance)
+    {
+        gameController = __instance;
+    }
 
-	[HarmonyPostfix]
-	[HarmonyPatch(typeof(PlayerController), "Update")]
-	private static void PlayerControllerUpdatePostPatch()
-	{
-		if (gameController == null)
-			return;
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlayerController), "Update")]
+    private static void PlayerControllerUpdatePostPatch()
+    {
+        if (gameController == null)
+            return;
 
-		Keyboard keyboard = Keyboard.current;
+        Keyboard keyboard = Keyboard.current;
 
-		if (keyboard == null)
-			return; // No keyboard connected.
+        if (keyboard == null)
+            return; // No keyboard connected.
 
-		if (keyboard.escapeKey.wasPressedThisFrame && paused)
-		{
-			if (gameController.CurrentState is PauseState)
-				gameController.pauseResumeButton.onClick.Invoke();
-			else if (gameController.CurrentState is OptionsState)
-				gameController.optionsBackButton.onClick.Invoke();
-			else if (gameController.CurrentState is SynergyUIState)
-				gameController.synergiesUIBackButton.onClick.Invoke();
-		}
-	}
+        if (keyboard.escapeKey.wasPressedThisFrame && paused)
+        {
+            if (gameController.CurrentState is PauseState)
+                gameController.pauseResumeButton.onClick.Invoke();
+            else if (gameController.CurrentState is OptionsState)
+                gameController.ChangeState<PauseState>();
+            else if (gameController.CurrentState is SynergyUIState)
+                gameController.synergiesUIBackButton.onClick.Invoke();
+        }
+    }
 }
